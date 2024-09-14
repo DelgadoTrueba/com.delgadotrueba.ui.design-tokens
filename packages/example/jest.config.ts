5ad6aed0dffed91b0c0cf61e4f98d530389ed8b6
Delgado Trueba <jose.delgado@babel.es>
2024-09-14 07:45:49 +0200

    chore: :building_construction: add build and linter and test

--- packages/example/jest.config.ts
+++ packages/example/jest.config.ts
@@ -2,6 +2,7 @@
 export default {
   displayName: '@delgadotrueba/example',
   preset: '../../jest.preset.js',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
