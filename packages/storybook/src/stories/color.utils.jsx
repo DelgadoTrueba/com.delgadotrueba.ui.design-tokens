9b2580a6385ad806fe77bef215a0149218c58ef6
Delgado Trueba <delgadotrueba@outlook.es>
2024-10-26 18:39:04 +0000

    feat: update theme story III

--- packages/storybook/src/stories/color.utils.jsx
+++ packages/storybook/src/stories/color.utils.jsx
@@ -1,9 +1,21 @@
-export const groupColorsByPrefix = (colors) => {
+export const filterObjectByString = (colors, searchString) => {
+    const filteredObj = {};
+    Object.keys(colors).forEach(key => {
+        if (key.includes(searchString)) {
+            filteredObj[key] = colors[key];
+            delete colors[key]; // Efecto secundario: elimina la propiedad del objeto original
+        }
+    });
+    return filteredObj;
+}
+
+export const groupColorsByPrefix = (colors, groupDeep = 4) => {
+
     const grouped = {};
     Object.entries(colors).forEach(([key, value]) => {
       let keyArr =  key.split('-')
 
-      while(keyArr.length > 4){
+      while(keyArr.length > groupDeep){
         keyArr = keyArr.slice(0, -1)
       }
       const prefix = keyArr.join('-');
