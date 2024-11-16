bbdc98ddcdddc989a2ec3a9c1fb7bc565e0b15e9
Delgado Trueba <delgadotrueba@outlook.es>
2024-11-16 21:11:16 +0000

    feat: add shadows

--- packages/storybook/src/stories/components/design-tokens/Shadow/Shadow.jsx
+++ packages/storybook/src/stories/components/design-tokens/Shadow/Shadow.jsx
@@ -1,17 +1,17 @@
-import './Shadow.css'; // Asegúrate de importar el archivo CSS
+import './Shadow.css';
 
 const ShadowItem = ({ label, value }) => {
   return (
     <div className="shadow" style={{boxShadow: value}}>
       <span class="shadow-label">var(--{label})</span>
-      <span class="shadow-label">bg-{label}</span>
+      <span class="shadow-label">shadow-{label.replace("dt-shadow-general-","")}</span>
     </div>
   );
 };
 
 export function Shadow({ shadowName, shadowData }) {
   return (
-    <div className="container">
+    <div className="container-shadows">
       <h1 class="subtitle">
         Shadow: <span class="title">{shadowName.replace('dt-', '')}</span>
       </h1>
--- packages/storybook/src/stories/components/design-tokens/Shadow/Shadow.utils.js
+++ packages/storybook/src/stories/components/design-tokens/Shadow/Shadow.utils.js
@@ -5,27 +5,7 @@ import {
 
 import common from '../../../../../../design-tokens/dist/css/dt-theme.common.json';
 
-const shadowsRaised = groupByPrefix(
-  filterObjectByString(common, 'shadow-raised'),
-  3
-);
-
-const shadowsFloating = groupByPrefix(
-  filterObjectByString(common, 'shadow-floating'),
-  3
-);
-
-const shadowsApp = groupByPrefix(filterObjectByString(common, 'shadow-app'), 3);
-
-const shadows = {
-    ...filterObjectByString(common, 'shadow-top'),
-    ...filterObjectByString(common, 'shadow-overlay'),
-    ...filterObjectByString(common, 'shadow-intense'),
-}
 
 export default {
-    shadows,
-  ...shadowsRaised,
-  ...shadowsFloating,
-  ...shadowsApp,
+  'shadow-general': filterObjectByString(common, 'shadow')
 };
