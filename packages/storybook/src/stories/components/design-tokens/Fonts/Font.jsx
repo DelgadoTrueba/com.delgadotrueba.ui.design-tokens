2095bb9d283a5c2df6bbc1ad08ce1e1a4faa96bc
Delgado Trueba <delgadotrueba@outlook.es>
2024-11-16 15:53:29 +0000

    feat: create font IV

--- packages/storybook/src/stories/components/design-tokens/Fonts/Font.jsx
+++ packages/storybook/src/stories/components/design-tokens/Fonts/Font.jsx
@@ -1,19 +1,26 @@
 import './Font.css';
 
 const FONT_WEIGHT_NAMES = {
-    200: 'extra-light',
+    200: 'thin',
     400: 'normal',
     500: 'medium',
     700: 'bold',
     800: 'extra-bold'
 };
 
-const FontBox = ({style, weight, className }) => {
+const FontBox = ({fontStyle, fontWeight, fontFamily }) => {
   return (
     <div class="font-box">
-      <p class={`font-sample ${className}`}>Aa</p>
-      <p class="font-label">style: {style}</p>
-      <p class="font-label">weight: {FONT_WEIGHT_NAMES[weight]}</p>
+      <p 
+        class={`font-sample`}
+        style={{
+            fontFamily,
+            fontStyle,
+            fontWeight
+        }}  
+      >Aa</p>
+      <p class="font-label">style: {fontStyle}</p>
+      <p class="font-label">weight: {FONT_WEIGHT_NAMES[fontWeight]}</p>
     </div>
   );
 };
@@ -22,16 +29,16 @@ export const Font = ({fontName, fontData}) => {
   return (
     <div class="container">
       <h1 class="subtitle">
-        Typeface: <span class="title">{fontName}</span>
+        Typeface: <span class="title">{fontName.replace('dt-typography-font-face-', '')}</span>
       </h1>
       <div class="font-weights">
         {Object.entries(fontData).map(([key, value]) => {
             return (
                 <FontBox
                     key={key}
-                    style={value["font-style"]}
-                    weight={value["font-weight"]}
-                    class={value.className}
+                    fontStyle={value["font-style"]}
+                    fontWeight={value["font-weight"]}
+                    fontFamily={value["font-family"]}
                 />
             )
         })}
--- packages/storybook/src/stories/components/design-tokens/Fonts/font.utils.jsx
+++ packages/storybook/src/stories/components/design-tokens/Fonts/font.utils.jsx
@@ -2,9 +2,11 @@ import {filterObjectByString, groupByPrefix} from "../../../../utils/themeJson"
 
 import _fonts from '../../../../../../design-tokens/dist/css/dt-theme.fonts.json';
 
+console.log({_fonts})
+
 const sans = groupByPrefix(
-  filterObjectByString(_fonts, 'typography-font-face-sans'),
-  5
+  filterObjectByString(_fonts, 'typography-font-face-open-sans'),
+  6
 );
 
 export const fonts = {
